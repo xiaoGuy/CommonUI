ed5ddf7aba2fdcbd864b570f589487b75558f450
hua <794956307@qq.com>
2017-07-24 23:11:47 +0800

    增加 ClearEditText 和 ClearAutoCompleteText

--- demo/src/main/java/com/exmple/xiaoguy/demo/MainActivity.java
+++ demo/src/main/java/com/exmple/xiaoguy/demo/MainActivity.java
@@ -1,14 +1,10 @@
 package com.exmple.xiaoguy.demo;
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.xiaoguy.commonui.view.ClearAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,26 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityInfo activityInfo;
-        String pkgName = getPackageName();
-        String clsName = getClass().getCanonicalName();
-        try {
-            activityInfo = getPackageManager().getActivityInfo(
-                    new ComponentName(pkgName, clsName), PackageManager.MATCH_DEFAULT_ONLY);
-        }
-        catch (NameNotFoundException e) {
-            e.printStackTrace();
-            Log.e("size", "get activity info failed: " + getPackageName() + getClass().getCanonicalName());
-            return;
-        }
-
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_soft_overlay:
-                startActivity(new Intent(this, SoftInputOverlayActivity.class));
-                break;
-        }
+        ClearAutoCompleteTextView ctv = (ClearAutoCompleteTextView) findViewById(R.id.clear_edit);
+        ctv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{"111", "1111", "11111"}));
     }
 }
