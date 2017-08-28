a63922074041eb11d783984295843318a14cb879
xiaogai <zhangqh@xinguangnet.com>
2017-08-28 15:53:10 +0800

    添加 FilterAdapter，让 AutoCompleteTextView 自定义过滤规则更加方便

--- demo/src/main/java/com/exmple/xiaoguy/demo/MainActivity.java
+++ demo/src/main/java/com/exmple/xiaoguy/demo/MainActivity.java
@@ -1,38 +1,39 @@
 package com.exmple.xiaoguy.demo;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ArrayAdapter;
 
 import com.xiaoguy.commonui.view.ClearAutoCompleteTextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private Thread mThread1;
-    private Thread mThread2;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"111", "1111", "11111"});
         ClearAutoCompleteTextView ctv = (ClearAutoCompleteTextView) findViewById(R.id.clear_edit);
-        ctv.setAdapter(new FilterAdapter());
-
-        mThread1 = Thread.currentThread();
-        new Handler().post(new Runnable() {
+        FilterAdapter<String> adapter = new FilterAdapter<String>() {
             @Override
-            public void run() {
-                mThread2 = Thread.currentThread();
-
-                Log.d("size", (mThread1 == mThread2) + "");
+            public List<String> performFiltering(CharSequence input, List<String> values) {
+                List<String> result = new ArrayList<>(values.size());
+                for (String str : values) {
+                    if (str.startsWith(input.toString())) {
+                        result.add(str);
+                    }
+                }
+                return result;
             }
-        });
+        };
 
+        for (int i = 0; i < 1000000; i ++) {
+            adapter.add("ba");
+        }
 
+        ctv.setAdapter(adapter);
     }
 
 }
--- library/src/main/java/com/xiaoguy/commonui/view/FilterAdapter.java
+++ library/src/main/java/com/xiaoguy/commonui/view/FilterAdapter.java
@@ -1,4 +1,4 @@
-package com.exmple.xiaoguy.demo;
+package com.xiaoguy.commonui.view;
 
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
